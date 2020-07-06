eb1be4e218abc950f686386f1b4e72ceac72a011
git config --global user.name <stevenpcurtis@hotmail.com>
2020-07-06 15:18:18 +0100

    added shell command

---
+++ Tips/BPShellCommand/BPShellCommand/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  BPShellCommand
+//
+//  Created by Steven Curtis on 06/07/2020.
+//  Copyright © 2020 Steven Curtis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let randomNumber = Int.random(in: 1..<5)
+        print (randomNumber)
+    }
+
+
+}
+
