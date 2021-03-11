4cbf697634bfbc7fc706554bb6b24371b3756828
Steven Curtis <stevenpcurtis@hotmail.com>
2021-03-11 18:19:30 +0100

    added scrollview

---
+++ UIScrollviewAutoLayoutInterfaceBuilding/README.md
@@ -0,0 +1,1 @@
+A repo to support the [Medium article](https://stevenpcurtis.medium.com/uiscrollview-with-auto-layout-implemented-in-interface-builder-5338c60db17a)
---
+++ UIScrollviewAutoLayoutInterfaceBuilding/ScrollViewExample/ScrollViewExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollViewExample
+//
+//  Created by Steven Curtis on 07/04/2019.
+//  Copyright © 2019 Steven Curtis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
